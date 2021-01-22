7ea8959fa6385dbd9010818a1c539431db8d22bb
Uğur <uguraydn23@gmail.com>
2021-01-22 22:06:00 +0300

    Added Generics , repeat generics again

---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,41 @@
+﻿using DocumentFormat.OpenXml.Spreadsheet;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsIntro
+{
+    // T yazmamızın sebebi generic yapıyoruz . New ile ne istersen onu yaparsın
+    // string yada int kullanabilirsin  bu şekilde
+    class MyList<T>
+    {
+        T[] items;
+        //ctor shortcut
+     
+        
+        //constructor
+        // class i new lersen otomatik çalışıyor
+        public MyList()
+        {
+            items = new T[0];
+        }
+
+
+        public void Add(T item)
+        {
+
+            T[] tempArray = new items;
+
+            // Elimizde 0 değerinde list var.
+            // Listeni 1 arttır diyoruz
+            // Mevcuttakini 1 arttırıyor
+            items = new T[items.Length+1];
+
+
+            for (int i = 0; i < tempArray.Length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+        }
+    }
+}
---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GenericsIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Generics!");
+
+
+            //string yerine int de verebilirsin
+            MyList<string> isimler = new MyList<string>();
+            isimler.Add("Heyooo Generics");
+
+
+
+
+        }
+    }
+}
